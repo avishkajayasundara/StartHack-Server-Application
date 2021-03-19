6167fd0bd0b1a1414dffa46018973464e6880f56
cmdrGuyson <gayangakuruppu@gmail.com>
2021-03-20 04:37:13 +0530

    [+] Create admin dev route, login as admin

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const http = require("http");
 
+const { admin_create, admin_login } = require("./controllers/users");
+
 const app = express();
 
 app.use(cors());
@@ -10,5 +12,8 @@ app.use(express.json());
 
 /* Log HTTP requests */
 app.use(morgan("dev"));
+
+app.post("/admin-create", admin_create);
+app.post("/admin-login", admin_login);
 
 module.exports = http.createServer(app);
